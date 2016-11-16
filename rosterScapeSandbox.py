72faade284b518e63c88f8a25c982194525a7d4a
Adrian <adrianfakstorp@gmail.com>
2016-11-16 15:12:26 -0800

    Adding start file
    Preliminary code

---
+++ rosterScapeSandbox.py
@@ -0,0 +1,17 @@
+from pyquery import PyQuery as pq
+#from lxml import etree
+import urllib
+
+d = pq(url='http://wiki.teamliquid.net/dota2/Alliance')
+
+totalRows = len(d(".activesquad tr"))
+
+for num in range(2,totalRows):
+    name = d(".activesquad tr").eq(num).find('td').eq(2).html()
+    nickname = d(".activesquad tr").eq(num).find('td a').eq(1).html()
+    position = d(".activesquad tr").eq(num).find('td').eq(3).html()
+    if str(d(".activesquad tr").eq(num).find('td').eq(1).find('img')).find("Captain") > -1:
+        print "Captain"
+    print nickname
+    print name
+    print position
